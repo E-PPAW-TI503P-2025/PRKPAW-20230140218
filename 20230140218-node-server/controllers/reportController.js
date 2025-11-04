0f8e3d84807452576c1069c56558e568c646677a
nayisntaround87 <naylasalwahayati@gmail.com>
2025-11-04 19:19:04 +0700

    Add Tugas5

--- 20230140218-node-server/controllers/reportController.js
+++ 20230140218-node-server/controllers/reportController.js
@@ -1,9 +1,34 @@
+const { Presensi } = require("../models");
+const { Op } = require("sequelize");
 const presensiRecords = require("../data/presensiData");
 
-exports.getDailyReport = (req, res) => {
-  console.log("Controller: Mengambil data laporan harian dari array...");
-  res.json({
-    reportDate: new Date().toLocaleDateString(),
-    data: presensiRecords,
-  });
+exports.getDailyReport = async (req, res) => {
+  try {
+    const { nama, tanggalMulai, tanggalSelesai } = req.query;
+    let options = { where: {} };
+
+    if (nama) {
+      options.where.nama = {
+        [Op.like]: `%${nama}%`,
+      };
+    }
+
+    // Tambahan: filter berdasarkan rentang tanggal
+    if (tanggalMulai && tanggalSelesai) {
+      options.where.tanggal = {
+        [Op.between]: [tanggalMulai, tanggalSelesai],
+      };
+    }
+
+    const records = await Presensi.findAll(options);
+
+    res.json({
+      reportDate: new Date().toLocaleDateString(),
+      data: records,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Gagal mengambil laporan", error: error.message });
+  }
 };
--- 20230140218-node-server/routes/presensi.js
+++ 20230140218-node-server/routes/presensi.js
@@ -9,5 +9,7 @@ router.use(addUserData);
 // Endpoint presensi
 router.post('/check-in', presensiController.CheckIn);
 router.post('/check-out', presensiController.CheckOut);
+router.delete('/:id', presensiController.deletePresensi);
+router.put('/:id', presensiController.updatePresensi);
 
 module.exports = router;
